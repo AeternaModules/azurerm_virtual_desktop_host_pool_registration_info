cb12dc88997ec1843133683793e4d3026982babe
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:16:54 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_desktop_host_pool_registration_infos" {
+  description = "All virtual_desktop_host_pool_registration_info resources"
+  value       = azurerm_virtual_desktop_host_pool_registration_info.virtual_desktop_host_pool_registration_infos
+}
+
