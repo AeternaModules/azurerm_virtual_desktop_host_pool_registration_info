3da3eae664d100d2359d115e2cc9affce0ff42cd
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "virtual_desktop_host_pool_registration_infos_id" {
+  description = "Map of id values across all virtual_desktop_host_pool_registration_infos, keyed the same as var.virtual_desktop_host_pool_registration_infos"
+  value       = { for k, v in azurerm_virtual_desktop_host_pool_registration_info.virtual_desktop_host_pool_registration_infos : k => v.id }
+}
 output "virtual_desktop_host_pool_registration_infos_expiration_date" {
   description = "Map of expiration_date values across all virtual_desktop_host_pool_registration_infos, keyed the same as var.virtual_desktop_host_pool_registration_infos"
   value       = { for k, v in azurerm_virtual_desktop_host_pool_registration_info.virtual_desktop_host_pool_registration_infos : k => v.expiration_date }
