258544dda9c2ae4883f0120fa6bde0963b804402
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,14 @@
-output "virtual_desktop_host_pool_registration_infos" {
-  description = "All virtual_desktop_host_pool_registration_info resources"
-  value       = azurerm_virtual_desktop_host_pool_registration_info.virtual_desktop_host_pool_registration_infos
-  sensitive   = true
-}
 output "virtual_desktop_host_pool_registration_infos_expiration_date" {
-  description = "List of expiration_date values across all virtual_desktop_host_pool_registration_infos"
-  value       = [for k, v in azurerm_virtual_desktop_host_pool_registration_info.virtual_desktop_host_pool_registration_infos : v.expiration_date]
+  description = "Map of expiration_date values across all virtual_desktop_host_pool_registration_infos, keyed the same as var.virtual_desktop_host_pool_registration_infos"
+  value       = { for k, v in azurerm_virtual_desktop_host_pool_registration_info.virtual_desktop_host_pool_registration_infos : k => v.expiration_date }
 }
 output "virtual_desktop_host_pool_registration_infos_hostpool_id" {
-  description = "List of hostpool_id values across all virtual_desktop_host_pool_registration_infos"
-  value       = [for k, v in azurerm_virtual_desktop_host_pool_registration_info.virtual_desktop_host_pool_registration_infos : v.hostpool_id]
+  description = "Map of hostpool_id values across all virtual_desktop_host_pool_registration_infos, keyed the same as var.virtual_desktop_host_pool_registration_infos"
+  value       = { for k, v in azurerm_virtual_desktop_host_pool_registration_info.virtual_desktop_host_pool_registration_infos : k => v.hostpool_id }
 }
 output "virtual_desktop_host_pool_registration_infos_token" {
-  description = "List of token values across all virtual_desktop_host_pool_registration_infos"
-  value       = [for k, v in azurerm_virtual_desktop_host_pool_registration_info.virtual_desktop_host_pool_registration_infos : v.token]
+  description = "Map of token values across all virtual_desktop_host_pool_registration_infos, keyed the same as var.virtual_desktop_host_pool_registration_infos"
+  value       = { for k, v in azurerm_virtual_desktop_host_pool_registration_info.virtual_desktop_host_pool_registration_infos : k => v.token }
   sensitive   = true
 }
 
